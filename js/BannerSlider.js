e241e12def38affe59bbef11f7225054697b082c
nhuttien123 <89700239+nhuttien123@users.noreply.github.com>
2021-11-30 09:34:56 +0700

    first commit

---
+++ js/BannerSlider.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".js-banner-slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1900,
+    fade: true,
+  });
+});
